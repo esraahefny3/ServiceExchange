a05d0a80727c54f627b5477b6c5637fc74868ec0
AhmedMubarak <Altysh@DESKTOP-99B5TDO>
2018-05-29 17:47:50 +0200

    UserChallange InterFace

---
+++ ServiceExchange/src/main/java/com/service_exchange/api_services/dao/user/userChallange/UserChallangeImpl.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.service_exchange.api_services.dao.user.userChallange;
+
+import com.service_exchange.api_services.dao.user.UserInterFace;
+import com.service_exchange.entities.Challenge;
+import com.service_exchange.entities.UserTable;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author Altysh
+ */
+public class UserChallangeImpl implements UserChallangeInterFace {
+
+    @Autowired
+    UserInterFace userInterFace;
+
+    @Override
+    public Boolean addChallangeToUser(Integer chId, Integer userId) {
+
+        UserTable user = userInterFace.getUser(userId);
+
+        if (user != null) {
+            boolean state = user.addChallange(chId);
+            if (state) {
+                userInterFace.updateUser(user);
+
+            }
+            return state;
+        }
+
+        return false;
+    }
+
+    @Override
+    public Boolean removeChallangeToUser(Integer chId, Integer userId) {
+        UserTable user = userInterFace.getUser(userId);
+
+        if (user != null) {
+            boolean state = user.removeChallange(chId);
+            if (state) {
+                userInterFace.updateUser(user);
+
+            }
+            return state;
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<Challenge> getAllChallangeForUser(Integer userId) {
+        UserTable user = userInterFace.getUser(userId);
+        if (user != null) {
+            return user.getUserChallengeCollection().stream()
+                    .map((t) -> t.getChallenge()).collect(Collectors.toList());
+        }
+        return null;
+    }
+
+}
