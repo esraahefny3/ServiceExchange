3036c74a14e75a80bbc46363ebf9e656f25d406e
Nouran <nouransamak@gmail.com>
2018-06-04 17:47:15 +0200

    Notifications

---
+++ ServiceExchange/src/main/java/com/service_exchange/api_services/dao/notification/NotificationDataInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.service_exchange.api_services.dao.notification;
+
+import com.service_exchange.entities.Notification;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Nouran
+ */
+@Repository
+public interface NotificationDataInterface extends CrudRepository<Notification, Integer> {
+
+}
