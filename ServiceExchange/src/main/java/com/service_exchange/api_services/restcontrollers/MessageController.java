90e922215b21afc23b45c983200c644736c7e2cb
esraahefny <esraahefny3@gmail.com>
2018-06-05 11:54:56 +0300

    after documentation esraa

--- ServiceExchange/src/main/java/com/service_exchange/api_services/restcontrollers/MessageController.java
+++ ServiceExchange/src/main/java/com/service_exchange/api_services/restcontrollers/MessageController.java
@@ -27,6 +27,7 @@ import java.util.List;
  * @author esraa
  */
 @RestController
+@RequestMapping("/message")
 public class MessageController {
 
     @Autowired
@@ -127,7 +128,7 @@ public class MessageController {
         return null;
     }
     
-     @RequestMapping(value = "/getAllTransactionMessages/{transactio nId}/{pageNum}", method = RequestMethod.GET)
+     @RequestMapping(value = "/getAllTransactionMessages/{transactionId}/{pageNum}", method = RequestMethod.GET)
     public List<MessageTransactionDto> getAllTransactionMessages(@PathVariable("transactionId") Integer transactionId, @PathVariable("pageNum") Integer pageNum) {
         if (transactionId != null && pageNum != null) {
             return messageServiceInterfaceImpl.getAllTransactionMessages(transactionId, pageNum);
