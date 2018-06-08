172bd6c63e630f42f9a852e2a7da65157b6dca12
Nouran <nouransamak@gmail.com>
2018-06-08 15:48:31 +0200

    Add states in TransactionInfo

--- ServiceExchange/src/main/java/com/service_exchange/api_services/bussinessdaodelegates/transaction/transaction2/TransactionDelegateInterfaceNour.java
+++ ServiceExchange/src/main/java/com/service_exchange/api_services/bussinessdaodelegates/transaction/transaction2/TransactionDelegateInterfaceNour.java
@@ -1,4 +1,12 @@
 package com.service_exchange.api_services.bussinessdaodelegates.transaction.transaction2;
 
+import com.service_exchange.entities.Service;
+import com.service_exchange.entities.UserTable;
+
 public interface TransactionDelegateInterfaceNour {
+
+    UserTable checkIfUserExists(Integer userId);
+
+    Service checkIfServiceExists(Integer serviceId);
+
 }
--- ServiceExchange/src/main/java/com/service_exchange/api_services/bussinesslayer/transactionbussiness/TransactionServiceInterfaceNour.java
+++ ServiceExchange/src/main/java/com/service_exchange/api_services/bussinesslayer/transactionbussiness/TransactionServiceInterfaceNour.java
@@ -1,4 +1,7 @@
 package com.service_exchange.api_services.bussinesslayer.transactionbussiness;
 
+import com.service_exchange.api_services.dao.transaction.nour.TransactionDtoNour;
+
 public interface TransactionServiceInterfaceNour {
+    TransactionDtoNour makeTransactionOnService(TransactionDtoNour transactionDto);
 }
