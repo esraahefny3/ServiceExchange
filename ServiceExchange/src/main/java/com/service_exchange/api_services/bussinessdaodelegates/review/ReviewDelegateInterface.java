94601ff3063405dca0954381d10e3fb34cb3ffe4
Nouran <nouransamak@gmail.com>
2018-06-14 15:08:44 +0200

    Review classes and interfaces

---
+++ ServiceExchange/src/main/java/com/service_exchange/api_services/bussinessdaodelegates/review/ReviewDelegateInterface.java
@@ -0,0 +1,4 @@
+package com.service_exchange.api_services.bussinessdaodelegates.review;
+
+public interface ReviewDelegateInterface {
+}
---
+++ ServiceExchange/src/main/java/com/service_exchange/api_services/dao/review/ReviewDaoInterface.java
@@ -0,0 +1,4 @@
+package com.service_exchange.api_services.dao.review;
+
+public interface ReviewDaoInterface {
+}
